c74ed0b019afbc8d13e888034c8a8f6ec0342973
Eli Bennet <elibennett@elis-mbp-2.bisonnet.lipscomb.edu>
2019-03-07 11:34:28 -0600

    Card Class Connection Complete

--- goFish/Classes/cardDeck.swift
+++ goFish/Classes/cardDeck.swift
@@ -75,6 +75,68 @@ let heartsKing = UIImage(named: "KH.png")
     
 //Setting Cards up through Card Class
 
+// Spade Card Class
+let fspaceAce = Card(number: 0, PSuit: Suit.Spade, uiImage: spadeAce!)
+let fspade2 = Card(number: 2, PSuit: Suit.Spade, uiImage: spade2!)
+let fspade3 = Card(number: 3, PSuit: Suit.Spade, uiImage: spade3!)
+let fspade4 = Card(number: 4, PSuit: Suit.Spade, uiImage: spade4!)
+let fspade5 = Card(number: 5, PSuit: Suit.Spade, uiImage: spade5!)
+let fspade6 = Card(number: 6, PSuit: Suit.Spade, uiImage: spade6!)
+let fspade7 = Card(number: 7, PSuit: Suit.Spade, uiImage: spade7!)
+let fspade8 = Card(number: 8, PSuit: Suit.Spade, uiImage: spade8!)
+let fspade9 = Card(number: 9, PSuit: Suit.Spade, uiImage: spade9!)
+let fspade10 = Card(number: 10, PSuit: Suit.Spade, uiImage: spade10!)
+let fspadeJack = Card(number: 11, PSuit: Suit.Spade, uiImage: spadeJack!)
+let fspadeQueen = Card(number: 12, PSuit: Suit.Spade, uiImage: spadeQueen!)
+let fspadeKing = Card(number: 13, PSuit: Suit.Spade, uiImage: spadeKing!)
+
+
+//Diamonds Card Class
+let fdiaAce = Card(number: 0, PSuit: Suit.Diamond, uiImage: diaAce!)
+let fdia2 = Card(number: 2, PSuit: Suit.Diamond, uiImage: dia2!)
+let fdia3 = Card(number: 3, PSuit: Suit.Diamond, uiImage: dia3!)
+let fdia4 = Card(number: 4, PSuit: Suit.Diamond, uiImage: dia4!)
+let fdia5 = Card(number: 5, PSuit: Suit.Diamond, uiImage: dia5!)
+let fdia6 = Card(number: 6, PSuit: Suit.Diamond, uiImage: dia6!)
+let fdia7 = Card(number: 7, PSuit: Suit.Diamond, uiImage: dia7!)
+let fdia8 = Card(number: 8, PSuit: Suit.Diamond, uiImage: dia8!)
+let fdia9 = Card(number: 9, PSuit: Suit.Diamond, uiImage: dia9!)
+let fdia10 = Card(number: 10, PSuit: Suit.Diamond, uiImage: dia10!)
+let fdiaJack = Card(number: 11, PSuit: Suit.Diamond, uiImage: diaJack!)
+let fdiaQueen = Card(number: 12, PSuit: Suit.Diamond, uiImage: diaQueen!)
+let fdiaKing = Card(number: 13, PSuit: Suit.Diamond, uiImage: diaKing!)
+
+
+//Clubs Card Class
+let fclubsAce = Card(number: 0, PSuit: Suit.Club, uiImage: clubsAce!)
+let fclubs2 = Card(number: 2, PSuit: Suit.Club, uiImage: clubs2!)
+let fclubs3 = Card(number: 3, PSuit: Suit.Club, uiImage: clubs3!)
+let fclub4 = Card(number: 4, PSuit: Suit.Club, uiImage: clubs4!)
+let fclub5 = Card(number: 5, PSuit: Suit.Club, uiImage: clubs5!)
+let fclub6 = Card(number: 6, PSuit: Suit.Club, uiImage: clubs6!)
+let fclub7 = Card(number: 7, PSuit: Suit.Club, uiImage: clubs7!)
+let fclub8 = Card(number: 8, PSuit: Suit.Club, uiImage: clubs8!)
+let fclub9 = Card(number: 9, PSuit: Suit.Club, uiImage: clubs9!)
+let fclub10 = Card(number: 10, PSuit: Suit.Club, uiImage: clubs10!)
+let fclubJack = Card(number: 11, PSuit: Suit.Club, uiImage: clubsJack!)
+let fclubQueen = Card(number: 12, PSuit: Suit.Club, uiImage: clubsQueen!)
+let fclubKing = Card(number: 13, PSuit: Suit.Club, uiImage: clubsKing!)
+
+
+//Hearts Card Class
+let fheartsAce = Card(number: 0, PSuit: Suit.Heart, uiImage: heartsAce!)
+let fhearts2 = Card(number: 2, PSuit: Suit.Heart, uiImage: hearts2!)
+let fhearts3 = Card(number: 3, PSuit: Suit.Heart, uiImage: hearts3!)
+let fhearts4 = Card(number: 4, PSuit: Suit.Heart, uiImage: hearts4!)
+let fhearts5 = Card(number: 5, PSuit: Suit.Heart, uiImage: hearts5!)
+let fhearts6 = Card(number: 6, PSuit: Suit.Heart, uiImage: hearts6!)
+let fhearts7 = Card(number: 7, PSuit: Suit.Heart, uiImage: hearts7!)
+let fhearts8 = Card(number: 8, PSuit: Suit.Heart, uiImage: hearts8!)
+let fhearts9 = Card(number: 9, PSuit: Suit.Heart, uiImage: hearts9!)
+let fhearts10 = Card(number: 10, PSuit: Suit.Heart, uiImage: hearts10!)
+let fheartsJack = Card(number: 11, PSuit: Suit.Heart, uiImage: heartsJack!)
+let fheartsQueen = Card(number: 12, PSuit: Suit.Heart, uiImage: heartsQueen!)
+let fheartsKing = Card(number: 13, PSuit: Suit.Heart, uiImage: heartsKing!)
 
     
 
